d77d5fb55544dae31f33a8962d1c93054fad7176
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-11-30 10:47:11 +0000

    Completion of 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++){
 
+        let index = array.indexOf(arguments[i]);
+        if (index == -1){
+            continue;
+        }
+        arrayNew = array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
