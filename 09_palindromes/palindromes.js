423b217553837b0eafe6bc1409fa76128e7a8469
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-14 20:58:24 +0000

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringUpper = string.toUpperCase();
+    let array = Array.from(stringUpper);
+    for (let i=0; i < array.length; i++){
+        if (array[i] == " " || array[i]=="'"){
+            array.splice(i, 1);
+        }
+        if (array[i] == "," || array[i]=="?"|| array[i]=="."|| array[i]=="!"){
+            array.splice(i, 2);
+        }
+    }
+    return array.join('') == array.reverse().join('') ? true:false;
 };
 
 // Do not edit below this line
