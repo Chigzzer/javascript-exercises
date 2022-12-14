171a84ef12019b723b547dcb3282ea07f7439e22
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-14 21:31:59 +0000

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let age = [];
+    for (let i = 0; i < people.length; i++){
+        if (people[i].yearOfDeath == null){
+            people[i].yearOfDeath = (new Date()).getFullYear();
+        }
+        age[i] = people[i].yearOfDeath - people[i].yearOfBirth;
+    }
+    let oldest = age.sort((a, b) => a < b ? 1:-1)[0];
+    return people.find(person => (person.yearOfDeath - person.yearOfBirth) == oldest);
 };
 
 // Do not edit below this line
