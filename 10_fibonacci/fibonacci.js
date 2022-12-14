234ece6ba024102afc01f9e81d179c9544fb0286
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-14 21:09:09 +0000

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS";
+    }
+    let fib = [];
+    for (let i=0; i < a; i++){
+        if (i == 0 || i == 1){
+            fib[i] = 1;
+        }
+        else{
+        fib[i] = fib[i-1] + fib[i-2]; 
+        }
+    }
+    return fib[a-1];
 };
 
 // Do not edit below this line
