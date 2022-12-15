6eae54756914c88bcfdc919cec86a03e34525d60
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-15 21:35:38 +0000

    Completed 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,39 @@
-const caesar = function() {
+const caesar = function(string, number) {
+    let codedString= [];
+    console.log(string);
+    for (let i = 0; i < string.length; i++){
+        //console.log(number);
+        let lowerLimit;
+        let upperLimit;
+        if (string[i] == ' ' || string[i] == '.' || string[i] == ',' || string[i] == '!' || string[i] == '?' || string[i] == "'"){
+            codedString.push(string[i]);
+            continue;
+        }
+        else if (string[i] == string[i].toUpperCase()){
+            lowerLimit = 65;
+            upperLimit = 90;
+        }
+        else{
+            lowerLimit = 97;
+            upperLimit = 122;
+        }
+        let code = string[i].charCodeAt(0);
+        //console.log(`original code: ${code}`);
+        let caesarCode = code + number;
+        while (caesarCode < lowerLimit || caesarCode > upperLimit){
+            if (caesarCode < lowerLimit){
+                let overShot = lowerLimit - caesarCode - 1;
+                caesarCode = upperLimit - overShot; 
+            }
+            else if (caesarCode > upperLimit){
+                let overShot = caesarCode - upperLimit - 1;
+                caesarCode = lowerLimit + overShot; 
+            }
+        }       
+        codedString.push(String.fromCharCode(caesarCode));        
+    }
+    console.log(`string: ${codedString}`);
+    return codedString.join('');
 
 };
 
